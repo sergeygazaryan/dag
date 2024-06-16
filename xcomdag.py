3d2063630efc72655b12a86beeb7fe1766203198
sergeygazaryan <sergey.gazaryan@gmail.com>
2024-06-16 13:50:48 +0400

    Update xcomdag.py

--- xcomdag.py
+++ xcomdag.py
@@ -8,7 +8,7 @@ default_args = {
     'start_date': datetime(2024, 6, 15),
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
 
